8b93729f14d291a821278b5074e35fe357ae36d7
jani-nykanen <jnyknn@gmail.com>
2021-01-10 13:01:12 +0200

    Sword fighting

--- src/player.ts
+++ src/player.ts
@@ -13,12 +13,16 @@ class Player extends CollisionObject {
     private rollTimer : number;
     private faceDirection : Vector2;
 
+    private sprSword : Sprite;
+    private attacking : boolean;
+
 
     constructor(x : number, y : number) {
 
         super(x, y);
 
         this.spr = new Sprite(16, 16);
+        this.sprSword = new Sprite(16, 16);
 
         this.friction = new Vector2(0.1, 0.1);
         this.center = new Vector2(0, 7);
@@ -27,6 +31,7 @@ class Player extends CollisionObject {
         this.rollTimer = 0.0;let dir = new Vector2();
         this.faceDirection = new Vector2(0, 1);
 
+        this.attacking = false;
         this.flip = Flip.None;
     }
 
@@ -54,14 +59,33 @@ class Player extends CollisionObject {
     }
 
 
+    private swordAttack(ev : GameEvent) {
+        
+        if (ev.getAction("fire2") == State.Pressed) {
+
+            this.stopMovement();
+
+            this.spr.setFrame(0, this.spr.getRow() + 6);
+            this.sprSword.setFrame(3, this.spr.getRow());
+
+            this.attacking = true;
+
+            return true;
+        }
+
+        return false;
+    }
+
+
     private control(ev : GameEvent) {
 
         const BASE_SPEED = 1.0;
         const EPS = 0.01;
 
-        if (this.rolling) return;
+        if (this.rolling || this.attacking) return;
 
-        if (this.startRolling(ev))
+        if (this.startRolling(ev) ||
+            this.swordAttack(ev))
             return;
 
         this.target = ev.getStick().scalarMultiply(BASE_SPEED);
@@ -77,6 +101,8 @@ class Player extends CollisionObject {
         const EPS = 0.01;
         const BASE_RUN_SPEED = 12;
         const RUN_SPEED_MOD = 4;
+        const ATTACK_SPEED = 6;
+        const ROLL_SPEED = 4;
 
         // TODO: Fix the "bug" where the character won't get
         // animated but moves if the player keeps tapping
@@ -84,9 +110,23 @@ class Player extends CollisionObject {
 
         let animSpeed = 0;
 
+        if (this.attacking) {
+
+            this.spr.animate(this.spr.getRow(), 0, 3, ATTACK_SPEED, ev.step);
+            if (this.spr.getColumn() == 3) {
+
+                this.attacking = false;
+            }
+            else {
+
+                this.sprSword.setFrame(this.spr.getColumn() + 3, this.spr.getRow());
+                return;
+            }
+        }
+
         if (this.rolling) {
 
-            this.spr.animate(this.spr.getRow(), 0, 3, 4, ev.step);
+            this.spr.animate(this.spr.getRow(), 0, 3, ROLL_SPEED, ev.step);
             return;
         }
 
@@ -147,12 +187,60 @@ class Player extends CollisionObject {
     }
 
 
+    private drawSword(c : Canvas) {
+
+        let px = Math.round(this.pos.x);
+        let py = Math.round(this.pos.y);
+
+        // Might need this if the center changes
+        /*
+        let xoff = (this.center.x + this.spr.width/2);
+        let yoff = (this.center.y + this.spr.height/2);
+        */
+
+        // Also TODO: Numeric constants as `const ...`
+
+        let dir = this.flip == Flip.None ? 1 : -1;
+
+        switch(this.spr.getRow() % 3) {
+
+        case 0:
+
+            c.drawSprite(this.sprSword, c.getBitmap("player"),
+                px - 20 + 5 * (this.sprSword.getColumn()-3), 
+                py - 6 + 2 * (this.sprSword.getColumn()-3));
+            break;
+
+        case 1:
+
+            c.drawSprite(this.sprSword, c.getBitmap("player"),
+                px - 1 + 7 * (dir - 1) + dir * 3 * (this.sprSword.getColumn()-3), 
+                py - 24 + 4 * (this.sprSword.getColumn()-3),
+                this.flip);
+            break;
+
+        case 2:
+
+            c.drawSprite(this.sprSword, c.getBitmap("player"),
+                px + 6 - 6 * (this.sprSword.getColumn()-3), 
+                py - 20 - 2 * (this.sprSword.getColumn()-3));
+            break;
+
+        default:
+            break;
+        }
+    }
+
+
     public draw(c : Canvas) {
 
         let shadow = c.getBitmap("shadow");
 
         let px = Math.round(this.pos.x);
         let py = Math.round(this.pos.y);
+
+        let xoff = (this.center.x + this.spr.width/2);
+        let yoff = (this.center.y + this.spr.height/2);
 
         c.setGlobalAlpha(0.67);
         c.drawBitmapRegion(shadow, 
@@ -161,9 +249,19 @@ class Player extends CollisionObject {
             py - shadow.height/2);
         c.setGlobalAlpha();
 
+        // Sword, back
+        if (this.attacking && this.spr.getRow() % 3 > 0) {
+
+            this.drawSword(c);
+        }
+
         c.drawSprite(this.spr, c.getBitmap("player"),
-            px - (this.center.x + this.spr.width/2), 
-            py - (this.center.y + this.spr.height/2), 
-            this.flip);
+            px - xoff, py - yoff, this.flip);
+
+        // Sword, front
+        if (this.attacking && this.spr.getRow() % 3 == 0) {
+
+            this.drawSword(c);
+        }
     }
 }
