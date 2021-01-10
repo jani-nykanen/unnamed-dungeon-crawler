9ab1173501881184b74d7f0f8481bc9324f29aed
jani-nykanen <jnyknn@gmail.com>
2021-01-10 10:37:13 +0200

    Rolling

--- src/player.ts
+++ src/player.ts
@@ -9,6 +9,9 @@ class Player extends CollisionObject {
 
 
     private flip : Flip;
+    private rolling : boolean;
+    private rollTimer : number;
+    private faceDirection : Vector2;
 
 
     constructor(x : number, y : number) {
@@ -20,15 +23,52 @@ class Player extends CollisionObject {
         this.friction = new Vector2(0.1, 0.1);
         this.center = new Vector2(0, 7);
 
+        this.rolling = false;
+        this.rollTimer = 0.0;let dir = new Vector2();
+        this.faceDirection = new Vector2(0, 1);
+
         this.flip = Flip.None;
+    }
+
+
+    // Star trolling?
+    private startRolling(ev : GameEvent) {
+
+        const ROLL_SPEED = 1.5;
+        const ROLL_TIME = 30;
+    
+        if (ev.getAction("fire1") == State.Pressed) {
+
+            this.speed = this.faceDirection.clone().scalarMultiply(ROLL_SPEED);
+            this.target = this.speed.clone();
+
+            this.rolling = true;
+            this.rollTimer = ROLL_TIME;
+
+            this.spr.setFrame(0, this.spr.getRow() + 3);
+
+            return true;
+        }
+
+        return false;
     }
 
 
     private control(ev : GameEvent) {
 
         const BASE_SPEED = 1.0;
+        const EPS = 0.01;
+
+        if (this.rolling) return;
+
+        if (this.startRolling(ev))
+            return;
 
         this.target = ev.getStick().scalarMultiply(BASE_SPEED);
+        if (this.target.length() > EPS) {
+
+            this.faceDirection = this.target.normalize();
+        }
     }
 
 
@@ -42,8 +82,15 @@ class Player extends CollisionObject {
         // animated but moves if the player keeps tapping
         // keys
 
-        let row = this.spr.getRow();
         let animSpeed = 0;
+
+        if (this.rolling) {
+
+            this.spr.animate(this.spr.getRow(), 0, 3, 4, ev.step);
+            return;
+        }
+
+        let row = this.spr.getRow() % 3;
 
         // Determine direction (read: row)
         if (this.target.length() > EPS) {
@@ -70,10 +117,33 @@ class Player extends CollisionObject {
     }
 
 
+    private updateTimers(ev : GameEvent) {
+
+        const MINIMUM_ROLL_TIME = 10;
+
+        // Rolling
+        if (this.rollTimer > 0.0) {
+
+            if ((ev.getAction("fire1") & State.DownOrPressed) == 0) {
+
+                this.rollTimer = Math.min(this.rollTimer, MINIMUM_ROLL_TIME);
+            }
+
+            this.rollTimer -= ev.step;
+            if (this.rollTimer <= 0) {
+
+                this.rollTimer = 0;
+                this.rolling = false;
+            }
+        }
+    }
+
+
     protected updateLogic(ev : GameEvent) {
 
         this.control(ev);
         this.animate(ev);
+        this.updateTimers(ev);
     }
 
 
@@ -85,8 +155,10 @@ class Player extends CollisionObject {
         let py = Math.round(this.pos.y);
 
         c.setGlobalAlpha(0.67);
-        c.drawBitmap(c.getBitmap("shadow"), 
-            px - shadow.width/2, py - shadow.height/2);
+        c.drawBitmapRegion(shadow, 
+            0, 0, 16, 8,
+            px - shadow.width/4, 
+            py - shadow.height/2);
         c.setGlobalAlpha();
 
         c.drawSprite(this.spr, c.getBitmap("player"),
