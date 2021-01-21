8fa876c300794b65492238c2b8d9adef8e003ad6
jani-nykanen <jnyknn@gmail.com>
2021-01-21 17:26:46 +0200

    Skull slime, does not explode yet

--- src/enemytypes.ts
+++ src/enemytypes.ts
@@ -8,6 +8,7 @@
 const getEnemyList = () : Array<Function> => [
     Slime, Bat, Spider, Fly,
     Spook, Fungus, FatFungus, Apple,
+    SkullSlime,
 ];
 
 
@@ -532,4 +533,62 @@ class Apple extends Enemy {
         this.forward = !pl.isAttacking() && !pl.isReadyingSpinAttack();
     }
 
+}
+
+
+class SkullSlime extends Enemy {
+
+    
+    private dir : Vector2;
+
+
+    constructor(x : number, y : number) {
+
+        super(x, y, 9, 10);
+
+        this.shadowType = 1;
+        this.spr.setFrame((Math.random() * 4) | 0, this.spr.getRow());
+
+        this.friction = new Vector2(0.025, 0.025);
+        this.dir = new Vector2(0, 1);
+
+        this.radius = 4;
+        this.damage = 3;
+
+        this.hitbox = new Vector2(6, 4);
+        this.collisionBox = this.hitbox.clone();
+        this.damageBox = new Vector2(10, 10);
+    }
+
+
+    protected updateAI(ev : GameEvent) {
+        
+        const ANIM_SPEED = 12;
+        const RUSH_SPEED = 1.0;
+
+        let oldFrame = this.spr.getColumn();
+        this.spr.animate(this.spr.getRow(), 0, 3, ANIM_SPEED, ev.step);
+
+        if (oldFrame == 0 && this.spr.getColumn() != oldFrame) {
+
+            this.speed = Vector2.scalarMultiply(this.dir, RUSH_SPEED);
+            this.flip = this.speed.x < 0 ? Flip.None : Flip.Horizontal;
+        }   
+    }
+
+
+    protected playerEvent(pl : Player, ev : GameEvent) {
+
+        this.dir = Vector2.direction(this.pos, pl.getPos());
+    }
+
+
+    protected wallCollisionEvent(dirx : number, diry : number, ev : GameEvent) {
+
+        if (Math.abs(dirx) > 0)
+            this.speed.x *= -1;
+
+        if (Math.abs(diry) > 0)
+            this.speed.y *= -1;
+    }
 }
